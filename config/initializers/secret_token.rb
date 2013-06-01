cc1249d19a63955be919cf96ff70a5acf8014856
Roneesh Vashisht <roneesh@gmail.com>
2013-05-31 22:36:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hookah::Application.config.secret_token = '6a7144a00a7cec54fd748502e4fca848c6b3a135e261f2afda6339e6fe50162d60dd9f908e2d2308836e203edb326606c73269de5c10d8484c9b498973bb74be'
