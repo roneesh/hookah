3db537a87031ad73138240174579d84c54635693
Roneesh Vashisht <roneesh@gmail.com>
2013-06-01 00:17:21 -0500

    user model has password and email uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :city, :email, :name, :password_digest, :state, :street1, :street2, :zipcode
+  attr_accessible :city, :email, :name, :password_digest, :state, :street1, :street2, :zipcode, :password, :password_confirmation
+
+  has_secure_password
 
   has_many :boxes
 
-  validates_presence_of :city, :email, :name, :password_digest, :state, :street1, :street2, :zipcode
+  validates_presence_of :city, :name, :email, :state, :street1, :zipcode
+
+  validates :email, :uniqueness => true
 
 end
